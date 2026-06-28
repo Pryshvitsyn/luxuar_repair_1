a59c792fbbe8b0ea4c3b47e6e6ade9f656ff8204
Pryshvitsyn <pryshvitsyna@gmail.com>
2026-06-28 22:51:16 +0800

    Fix scroll lock restoration when closing ProjectDetailsModal

--- src/components/ProjectDetailsModal.jsx
+++ src/components/ProjectDetailsModal.jsx
@@ -7,9 +7,15 @@ const ProjectDetailsModal = ({ project, onClose }) => {
     const { t } = useLanguage();
 
     useEffect(() => {
+        const previousBodyOverflow = document.body.style.overflow;
+        const previousHtmlOverflow = document.documentElement.style.overflow;
+
         document.body.style.overflow = 'hidden';
+        document.documentElement.style.overflow = 'hidden';
+
         return () => {
-            document.body.style.overflow = '';
+            document.body.style.overflow = previousBodyOverflow;
+            document.documentElement.style.overflow = previousHtmlOverflow;
         };
     }, []);
 
