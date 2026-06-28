36a8cd1154326d8b76cd58ec5369d2f8ead2a6cd
Pryshvitsyn <pryshvitsyna@gmail.com>
2026-06-29 00:43:13 +0800

    Fix Vite base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/luxuar_repair_1/',
+  base: '/',
   plugins: [
     tailwindcss(),
     react(),
